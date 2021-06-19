7ef9ec45ea40a5da331bab6132b315fd46c0fda5
Egenchango7 <echaname@bdo.com.pe>
2021-06-18 22:42:04 -0500

    change jquery to cdn

--- resources/views/layout/script.blade.php
+++ resources/views/layout/script.blade.php
@@ -1,4 +1,4 @@
-<script src="{{asset('js/jquery-3.6.0.js')}}"></script>
+<script src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script>
 <script src="https://code.jquery.com/ui/1.12.0/jquery-ui.js" integrity="sha256-0YPKAwZP7Mp3ALMRVB2i8GXeEndvCq3eSl/WsAl1Ryk=" crossorigin="anonymous"></script>
 <?=
 '<script>
